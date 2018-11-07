f9992378d329d836fc040bc82de7bcdb008d8c80
woozico0521 <zyy960521.com>
2018-11-07 20:58:50 +0800

    女装js

---
+++ js/a3.js
@@ -0,0 +1,15 @@
+(function(){/* 导航 */
+    var oNav=document.getElementById("nav");
+    
+    window.addEventListener("scroll",function(e){
+        
+        var t =document.documentElement.scrollTop||document.body.scrollTop; 
+        //alert("滚动了");
+        if(t>150){
+            oNav.className="down on clearfix";
+        }else{
+            oNav.className="down clearfix"
+        }
+    });
+})();
+    
